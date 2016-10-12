e2a4e7cfad8441c03b3a5fc2d1db788f72a645cb
cormullion <cormullion@mac.com>
2016-10-12 12:44:45 +0100

    updates for Luxor v0.5

--- src/draw-logo.jl
+++ src/draw-logo.jl
@@ -1,12 +1,10 @@
 #!/usr/bin/env julia
 
 using Luxor, Colors
-# find this file, eg:
-include("julia-logo.jl")
 Drawing(1000, 1000, "/tmp/julia-logo.pdf")
 origin()
 background("ivory")
 sethue("red")
-julialogo(false)
+julialogo()
 finish()
 preview()
